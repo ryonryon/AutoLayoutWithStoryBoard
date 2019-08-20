7750056955e263766e1f6c8e986649b30a54122c
ryotogashi <ryo.togashi.ca@gmail.com>
2019-08-20 09:48:53 -0700

    class material

---
+++ AutoLayoutWithStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutWithStoryBoard
+//
+//  Created by Ryo Togashi on 2019-08-19.
+//  Copyright © 2019 Ryo Togashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
